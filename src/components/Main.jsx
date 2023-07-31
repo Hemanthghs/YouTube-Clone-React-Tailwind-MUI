dc851330233b51e8e495f8e30932e0f8b476bd37
hemanthghs <ghshemanth@gmail.com>
2023-08-01 01:42:39 +0530

    feat: Add shorts page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Sidebar, { Options } from "./Sidebar";
 import Content from "./Content";
 import { Box } from "@mui/material";
+import Shorts from "./Shorts";
 
 export default function Main() {
   return (
     <div>
       <div>
         <Sidebar />
-        <Content />
+        {/* <Content /> */}
+        <Shorts />
       </div>
       <Box className="bg-[#0f0f0f] border-t-[1px] border-[#414141] w-full fixed bottom-0 flex items-center justify-around tablet:hidden">
         <Options />
