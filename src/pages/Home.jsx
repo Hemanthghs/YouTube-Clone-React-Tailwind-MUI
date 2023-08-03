2fceb9a17540448abe43cb8e1d81b45ad986a44d
hemanthghs <ghshemanth@gmail.com>
2023-08-04 01:39:44 +0530

    ui: shorts ui improvement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Main from '../components/Main';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Main from "../components/Main";
 
 export default function Home() {
   return (
-    <div className='bg-[#0f0f0f] text-white h-max tablet:mr-2'>
-        <Navbar />
-        <Main />
+    <div className="bg-[#0f0f0f] text-white h-max tablet:mr-2">
+      <Navbar />
+      <Main />
     </div>
-  )
+  );
 }
